163434c62aff2ecac878b1359981219abd4e564e
ionling <soarvision@outlook.com>
2023-08-15 09:55:48 +0800

    feat: Add command notification (#11)

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,24 +1,58 @@
-import {wait} from '../src/wait'
+import {exec} from '../src/exec'
+import * as lark from '../src/lark'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
-import {expect, test} from '@jest/globals'
+import {expect, test, describe} from '@jest/globals'
+import {log} from 'console'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
+describe('exec', () => {
+  test('exec echo', async () => {
+    const {stdout, stderr} = await exec('echo hello world')
+    expect(stdout).toEqual('hello world\n')
+    expect(stderr).toEqual('')
+  })
+
+  test('exec echo stderr', async () => {
+    const {stdout, stderr} = await exec('>&2 echo "so bad"')
+    expect(stdout).toEqual('')
+    expect(stderr).toEqual('so bad\n')
+  })
+
+  test('exec cat not found file', async () => {
+    try {
+      await exec('cat hello')
+    } catch (err) {
+      log('got err:', err)
+    }
+  })
 })
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
+test('new url', async () => {
+  const url = new URL('https://open.feishu.cn/open-apis/bot/v2/hook/xxxx')
+  expect(url.protocol).toEqual('https:')
+  expect(url.host).toEqual('open.feishu.cn')
+  expect(url.pathname).toEqual('/open-apis/bot/v2/hook/xxxx')
+})
+
+test('match', () => {
+  const regex = new RegExp(`^refs/pull/(.+)/merge$`)
+  const matches = 'refs/pull/11/merge'.match(regex)
+  expect(matches?.at(1)).toEqual('11')
+})
+
+test.skip('lark', async () => {
+  const card = new lark.Card(lark.CardTemplate.Violet, 'HELLO')
+  card.addElements(new lark.Markdown('**some**thing'))
+  card.addElements(new lark.Text('text block here'))
+  lark.send(
+    'https://open.feishu.cn/open-apis/bot/v2/hook/xxxxx',
+    card.toMessage()
+  )
 })
 
 // shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
+test.skip('test runs', () => {
   process.env['INPUT_MILLISECONDS'] = '500'
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,78 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import * as lark from './lark'
+import {exec} from './exec'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const dryRun = core.getInput('dryRun'),
+      title = core.getInput('title'),
+      content = core.getInput('content'),
+      cmd = core.getInput('command'),
+      webhook = core.getInput('larkBotWebhook'),
+      ctx = github.context,
+      ownerRepo = `${ctx.repo.owner}/${ctx.repo.repo}`,
+      repoURL = `https://github.com/${ownerRepo}`,
+      actionURL = `${repoURL}/actions`
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    core.debug(`Got command: ${cmd}`)
 
+    let commitURL = `${repoURL}/commit/${ctx.sha}`,
+      refType: 'branch' | 'tag' | 'pull' | undefined,
+      pullURL: string | undefined
+    if (ctx.ref.startsWith('refs/heads/')) {
+      refType = 'branch'
+    } else if (ctx.ref.startsWith('refs/tags/')) {
+      refType = 'tag'
+    } else if (ctx.ref.startsWith('refs/pull')) {
+      refType = 'pull'
+      const regex = new RegExp(`^refs/pull/(.+)/merge$`)
+      const matches = 'refs/pull/11/merge'.match(regex)
+      pullURL = `${repoURL}/pull/${matches?.at(1)}`
+      commitURL = `${commitURL}/commits/${ctx.sha}`
+    }
+
+    let ciInfo = `[CI](${actionURL}) of ${ownerRepo}/${ctx.ref}, see`
+    if (refType === 'pull') {
+      ciInfo = ciInfo.concat(` [PR](${pullURL}),`)
+    }
+    ciInfo = ciInfo.concat(` [commit](${commitURL}).`)
+
+    let cmdInfo = '',
+      tpl = lark.CardTemplate.Turquoise
+    if (cmd) {
+      try {
+        const {stdout, stderr} = await exec(cmd)
+        if (stderr === '') {
+          cmdInfo = stdout
+        } else {
+          tpl = lark.CardTemplate.Violet
+          let text = ''
+          if (stdout !== '') {
+            text = text.concat(`STDOUT:\n${stdout}\n\n`)
+          }
+          cmdInfo = text.concat(`STDERR:\n${stderr}`)
+        }
+      } catch (err) {
+        tpl = lark.CardTemplate.Carmine
+        cmdInfo = `ERROR:\n${err}`
+      }
+    }
+    if (dryRun) {
+      core.info(`CI info: ${ciInfo}`)
+      if (content) core.info(`Content: ${content}`)
+      if (cmd) core.info(`Command: ${cmdInfo}`)
+      return
+    }
+
+    const elems: lark.Element[] = [new lark.Markdown(ciInfo)]
+    if (content) elems.push(new lark.Markdown(content))
+    if (cmd) elems.push(new lark.Text(cmdInfo))
+    await lark.send(
+      webhook,
+      new lark.Card(tpl, title).addElements(...elems).toMessage()
+    )
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
